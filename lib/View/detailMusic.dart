02c749486c062c38fee53d9f2fcc4a67c566beb4
djinodissingar <dissingardjino@icloud.com>
2022-05-05 15:28:38 +0200

    Mise à jour 05/05/2022 pause après midi

--- lib/View/detailMusic.dart
+++ lib/View/detailMusic.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:awesome_icons/awesome_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:musicipssi/Model/Music.dart';
+import 'package:musicipssi/library/lib.dart';
 import 'package:musicipssi/modelView/fondEcran.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class  detailMusic extends StatefulWidget{
   Music musique;
@@ -17,11 +21,88 @@ class  detailMusic extends StatefulWidget{
 
 class detailMusicState extends State<detailMusic>{
   //variable
-  double positionnement = 0;
+  statut lecture  = statut.stopped;
+  AudioPlayer audioPlayer = AudioPlayer();
+  Duration positionnement = Duration(seconds: 0);
+  late StreamSubscription positionStream;
+  late StreamSubscription stateStream;
+  Duration dureeTotal = Duration(seconds: 0);
+  double volumeSound = 0.5;
   bool isPlaying = false;
 
 
-  //
+  //Fonction
+  configurationPlayer(){
+    audioPlayer.setUrl(widget.musique.lienMusic);
+    positionStream = audioPlayer.onAudioPositionChanged.listen((event) {
+      setState(() {
+        positionnement = event;
+      });
+      audioPlayer.onDurationChanged.listen((event) {
+        setState(() {
+          print("durée total $event");
+          dureeTotal = event;
+        });
+      });
+      stateStream = audioPlayer.onPlayerStateChanged.listen((event) {
+          if(event == statut.playing){
+            setState(() async {
+              dureeTotal = audioPlayer.getDuration() as Duration;
+              lecture = statut.playing;
+            });
+          }
+          else if(event == statut.stopped){
+            setState(() {
+              lecture = statut.stopped;
+            });
+
+          }
+      }
+      ,onError:(message){
+         print("erreur : $message");
+         setState(() {
+           lecture = statut.stopped;
+           positionnement = const Duration(seconds: 0);
+           dureeTotal  = const Duration(seconds : 0);
+         });
+          }
+      );
+
+
+
+    });
+  }
+
+
+
+
+
+  //play
+  Future play() async {
+    await audioPlayer.play(widget.musique.lienMusic,position: positionnement,volume: volumeSound);
+  }
+
+
+  //pause
+  Future pause() async{
+    await audioPlayer.pause();
+
+  }
+
+
+  //avance rapide
+
+
+  //Retour en arrière
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    configurationPlayer();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -48,7 +129,7 @@ class detailMusicState extends State<detailMusic>{
         //Pochette
         Container(
           height: 300,
-          width: 200,
+          width: 450,
           decoration: BoxDecoration(
             image: DecorationImage(
               image: (widget.musique.pochette == null)?NetworkImage("https://firebasestorage.googleapis.com/v0/b/musicipssi.appspot.com/o/cover.jpg?alt=media&token=2371302d-bac7-415b-86ff-180d60643a5d"):NetworkImage(widget.musique.pochette!)
@@ -75,6 +156,7 @@ class detailMusicState extends State<detailMusic>{
 
         //Icon de lecture, de pause,avance rapide, retour en arrière
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             //fast_forward
             IconButton(
@@ -85,16 +167,24 @@ class detailMusicState extends State<detailMusic>{
             ),
 
             //
-            (isPlaying)? IconButton(
+            (lecture == statut.stopped)? IconButton(
               onPressed: (){
+                setState(() {
+                  lecture = statut.paused;
+                  play();
+                });
 
               },
-              icon: const Icon(FontAwesomeIcons.pause),
+              icon: const Icon(FontAwesomeIcons.play,size: 40,),
             ): IconButton(
               onPressed: (){
+                setState(() {
+                  lecture = statut.stopped;
+                  pause();
+                });
 
               },
-              icon: const Icon(FontAwesomeIcons.play),
+              icon: const Icon(FontAwesomeIcons.pause,size: 40,),
             ),
 
 
@@ -118,18 +208,25 @@ class detailMusicState extends State<detailMusic>{
 
         //Slider pour le lecteur
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text("temps de début"),
-            Slider(
-                value: positionnement,
-                onChanged: (newValue){
-                  setState(() {
-                    positionnement = newValue;
-                  });
-                }
-            ),
-            Text("Temps fin")
+            Text(positionnement.toString()),
+
+            Text(dureeTotal.toString())
           ],
+        ),
+
+        Slider(
+            min: 0.0,
+            max: dureeTotal.inSeconds.toDouble(),
+            value: positionnement.inSeconds.toDouble(),
+            onChanged: (newValue){
+              setState(() {
+                Duration time = Duration(seconds: newValue.toInt());
+                positionnement = time;
+
+              });
+            }
         ),
 
       ],
@@ -137,3 +234,4 @@ class detailMusicState extends State<detailMusic>{
   }
 
 }
+
--- lib/library/constant.dart
+++ lib/library/constant.dart
@@ -2,3 +2,5 @@ import 'package:musicipssi/Model/Profil.dart';
 
 late Profil MonUser;
 enum type {rap,soul,techno,rock,zouk,reggae,rnb,jazz}
+enum statut{playing,stopped,paused,rewind,forward}
+
